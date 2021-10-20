d00b184b84d3d07071cf650125d037d88b49baa0
wadson <wadsonmatos@yahoo.com.br>
2021-10-20 16:38:55 -0300

    Commit inicial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using LiveSearchDemoWithJQuery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiveSearchDemoWithJQuery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            StudentInfoEntities db = new StudentInfoEntities();
+            return View(db.StudentInfoes.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
